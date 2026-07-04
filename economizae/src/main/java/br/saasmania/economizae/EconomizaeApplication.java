ec273d58723f5e3fa54188ff55a30820b4ef3977
AnesioSousa <anesios98@gmail.com>
2026-07-03 23:40:53 -0300

    feat: adicao da estrutura inicial do projeto

---
+++ economizae/src/main/java/br/saasmania/economizae/EconomizaeApplication.java
@@ -0,0 +1,13 @@
+package br.saasmania.economizae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomizaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomizaeApplication.class, args);
+	}
+
+}
---
+++ economizae/src/test/java/br/saasmania/economizae/EconomizaeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.saasmania.economizae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomizaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
